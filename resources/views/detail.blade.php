249dcb50e25e2cbeb57196b1f79a8b9dcf308858
destroylord <masapin68@gmail.com>
2024-06-13 12:53:49 +0700

    style: landing pages and detail

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('content')
+<div class="untree_co-section product-section before-footer-section">
+    <div class="container">
+        <div class="row">
+
+            <!-- Start Column 1 -->
+            <div class="col-12 col-md-4 col-lg-3 mb-5">
+                <a class="product-item" href="#">
+                    <img src="/assets/images/product-3.png" class="img-fluid product-thumbnail">
+                    <h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>
+
+                    <span class="icon-cross">
+                        <img src="/assets/images/cross.svg" class="img-fluid">
+                    </span>
+                </a>
+            </div> 
+            <!-- End Column 1 -->
+
+        </div>
+    </div>
+</div>
+@endsection
