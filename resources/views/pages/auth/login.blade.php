b80aad0d6e53564165f83df6283b10e3e3423fa3
destroylord <masapin68@gmail.com>
2024-06-25 16:17:43 +0700

    style: login & register

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,32 +1,38 @@
-@extends('layouts.auth')
+@extends('layouts.app')
 
 @section('content')
 @if (session()->has('alert_s'))
 <p>{{ session('alert_s') }}</p>
 @endif
 
-<form action="" method="post">
-    @csrf
-    <div>
-        <label for="email">Email</label>
-        <input id="email" type="text" name="email">
-
-        @error('email')
-            <small>{{ $message }}</small>
-        @enderror
+<div class="row">
+    <div class="col-lg-6 mx-auto mt-4 mb-4">
+        <div class="card">
+            <div class="card-header">
+                <h4>Login</h4>
+            </div>
+            <div class="card-body">
+                <form action="/login" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input id="email" class="form-control mb-2" type="text" name="email">
+                        @error('email')
+                            <small>{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input id="password" class="form-control mb-2" type="password" name="password">
+                        @error('password')
+                            <small>{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-2">Login</button>
+                </form>
+                <p>Belum punya akun? Silahkan klik <a href="{{ route('register') }}">Daftar</a></p>
+            </div>
+        </div>
     </div>
-    <div>
-        <label for="password">Password</label>
-        <input id="password" type="password" name="password">
-
-        @error('password')
-            <small>{{ $message }}</small>
-        @enderror
-    </div>
-    <div>
-        <button type="submit">Login</button>
-    </div>
-</form>
-
-<p>Belum punya akun? <a href="{{ route('register') }}">Daftar</a></p>
+</div>
 @endsection
--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -1,42 +1,54 @@
-@extends('layouts.auth')
+@extends('layouts.app')
 
 @section('content')
-<form action="" method="post">
-    @csrf
-    <div>
-        <label for="name">Nama Lengkap</label>
-        <input id="name" type="text" name="name" value="{{ old('name') }}">
 
-        @error('name')
-            <small>{{ $message }}</small>
-        @enderror
-    </div>
-    <div>
-        <label for="email">Email</label>
-        <input id="email" type="text" name="email" value="{{ old('email') }}">
+<div class="row">
+    <div class="col-lg-6 mx-auto mt-4 mb-4">
+        <div class="card">
+            <div class="card-header">
+                <h4>Registrasi</h4>
+            </div>
+            <div class="card-body">
+                <form action="" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Nama Lengkap</label>
+                        <input id="name" class="form-control" type="text" name="name" value="{{ old('name') }}">
 
-        @error('email')
-            <small>{{ $message }}</small>
-        @enderror
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input id="password" type="password" name="password">
+                        @error('name')
+                            <small>{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input id="email" class="form-control" type="text" name="email" value="{{ old('email') }}">
 
-        @error('password')
-            <small>{{ $message }}</small>
-        @enderror
-    </div>
-    <div>
-        <label for="password_confirmation">Konfirmasi Password</label>
-        <input id="password_confirmation" type="password" name="password_confirmation">
+                        @error('email')
+                            <small>{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input id="password" class="form-control" type="password" name="password">
 
-        @error('password_confirmation')
-            <small>{{ $message }}</small>
-        @enderror
+                        @error('password')
+                            <small>{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="password_confirmation">Konfirmasi Password</label>
+                        <input id="password_confirmation" class="form-control" type="password" name="password_confirmation">
+
+                        @error('password_confirmation')
+                            <small>{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Daftar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-    <div>
-        <button type="submit">Daftar</button>
-    </div>
-</form>
+</div>
 @endsection
