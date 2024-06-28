bca1474be04f3845b6f0245026f1c5bb01c6762f
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2024-06-28 14:47:13 +0700

    chore: refactor layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,45 @@
 @extends('layouts.app')
 
-@section('content')		
-	<!-- Start Hero Section -->
-		<div class="hero">
-			<div class="container">
-				<div class="row justify-content-between">
-					<div class="col-lg-5">
-						<div class="intro-excerpt">
-							<h1>Studio Desain Interior Modern <span class="d-block">Segera Hadir!</span></h1>
-							<p class="mb-4">Menyajikan desain interior yang menarik dan menenangkan untuk ruangan Anda. Daftar sekarang dan rasakan kebahagian dari ruangan impian Anda!</p>
+@section('hero')
+<div class="hero">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-lg-5">
+                <div class="intro-excerpt">
+                    <h1>Studio Desain Interior Modern <span class="d-block">Segera Hadir!</span></h1>
+                    <p class="mb-4">Menyajikan desain interior yang menarik dan menenangkan untuk ruangan Anda. Daftar sekarang dan rasakan kebahagian dari ruangan impian Anda!</p>
 
-						</div>
-					</div>
-					<div class="col-lg-7">
-						<div class="hero-img-wrap">
-							<img src="/assets/images/couch.png" class="img-fluid">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- End Hero Section -->
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="hero-img-wrap">
+                    <img src="/assets/images/couch.png" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
 
-		<div class="untree_co-section product-section before-footer-section">
-		    <div class="container">
-		      	<div class="row">
+@section('content')
+<div class="untree_co-section product-section before-footer-section">
+    <div class="container">
+        <div class="row">
+            <!-- Start Column 1 -->
+            <div class="col-12 col-md-4 col-lg-3 mb-5">
+                <a class="product-item" href="#">
+                    <img src="/assets/images/product-3.png" class="img-fluid product-thumbnail">
+                    <h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>
 
-		      		<!-- Start Column 1 -->
-					<div class="col-12 col-md-4 col-lg-3 mb-5">
-						<a class="product-item" href="#">
-							<img src="/assets/images/product-3.png" class="img-fluid product-thumbnail">
-							<h3 class="product-title">Nordic Chair</h3>
-							<strong class="product-price">$50.00</strong>
+                    <span class="icon-cross">
+                        <img src="/assets/images/cross.svg" class="img-fluid">
+                    </span>
+                </a>
+            </div>
+            <!-- End Column 1 -->
 
-							<span class="icon-cross">
-								<img src="/assets/images/cross.svg" class="img-fluid">
-							</span>
-						</a>
-					</div> 
-					<!-- End Column 1 -->
-
-		      	</div>
-		    </div>
-		</div>
+        </div>
+    </div>
+</div>
 @endsection
